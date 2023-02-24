262481ff82160841310f35bbeb98357f6b9831f7
Daniel <danidavidov13.dd@gmail.com>
2023-02-24 16:37:57 +0200

    Setting up application.yml

--- src/main/java/bg/softuni/bookingApp/model/entity/Picture.java
+++ src/main/java/bg/softuni/bookingApp/model/entity/Picture.java
@@ -4,7 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "pictures")
 public class Picture extends BaseEntity {
